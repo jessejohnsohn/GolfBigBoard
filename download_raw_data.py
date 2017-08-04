62ebaa4050180385fab3d4604f379b5e7720c67f
jessejohnsohn <jesserjohnson@gmail.com>
2017-08-03 21:57:29 -0700

    Make it work perfectly

--- download_raw_data.py
+++ download_raw_data.py
@@ -1,8 +1,13 @@
 from bs4 import BeautifulSoup
+import urllib
 import requests
+import re
 
 def scrape(site): # basic scrape, takes URL as an argument
-    r = requests.get(site).text
+    if site[0:4] == 'http':
+        r = requests.get(site).text
+    else:
+        r = urllib.urlopen(site).read()
     return BeautifulSoup(r, "html.parser")
 
 def get_raw_data(): # get raw player data to parse for player_id's
@@ -20,9 +25,9 @@ def get_names_and_power_ranking(raw_data): # use raw player data to put player_i
         full_name = datum.get_text().encode('UTF8')
         first_name = str(full_name).strip().split(',')[1]
         last_name = str(full_name).strip().split(',')[0]
-        player["First Name"] = first_name
-        player["Last Name"] = last_name
-        player["Power Ranking"] = power_ranking
+        player["First Name"] = first_name.strip()
+        player["Last Name"] = last_name.strip()
+        player["Power Ranking"] = power_ranking.strip()
         players.append(player)
 
     return players
@@ -32,16 +37,48 @@ def write_to_file(players):
     for player in players:
         print("Downloading information for %s %s" % (player["First Name"], player["Last Name"]))
         ranking = player["Power Ranking"]
+        print(ranking)
         ranking_low = len(players)
-        file = open("scrapedata%s.txt" % ranking, "w")
-        if ranking < ranking_low:
+        file = open(player["Power Ranking"] + player["First Name"] + player["Last Name"] + ".txt", "w")
+        if int(ranking) < ranking_low:
             x = str(scrape(root + '?compare=%s,%d' % (ranking, ranking_low)))
         else:
+            print("Ranking == ranking_low" + ranking + " " + str(ranking_low))
             x = str(scrape(root + '?compare=%s,%d' % (ranking_low, 1)))
         file.write(x)
         file.close()
         print("Done!\n")
 
+def get_stat(players):
+    numbers = re.compile('\d+(?:\.\d+)?')
+    for player in players:
+        x = scrape(player["Power Ranking"] + player["First Name"] + player["Last Name"] + ".txt").findAll('tbody')
+        x = x[4].find_all('center')
+        holding = []
+        for i in x:
+            i = numbers.findall(str(i))
+            holding.append(i)
+        try:
+            player["Recent Performance"] = holding[0][0]
+        except:
+            player["Recent Performance"] = "NR"
+        try:
+            player["Course History"] = holding[2][0]
+        except:
+            player["Course History"] = "NR"
+        try:
+            player["Course Fit"] = holding[4][0]
+        except:
+            player["Course Fit"] = "NR"
+        try:
+            player["Similar Course"] = holding[6][0]
+        except:
+            player["Similar Course"] = "NR"
+
+    return players[0:len(players)]
+
 players = get_names_and_power_ranking(get_raw_data())
 
-write_to_file(players)
+# write_to_file(players)
+
+print(get_stat(players))
